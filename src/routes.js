40bcbbfe85917774b3e67f9552de596d5da4596e
luigiolivi <eterowiczluigi@gmail.com>
2023-05-24 18:14:38 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
+
+const routes = new Router()
+
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Rodolfo',
+    email: 'rodolfo@email.com',
+    password_hash: '23asd234',
+  })
+  return response.json(user)
+})
+
+export default routes

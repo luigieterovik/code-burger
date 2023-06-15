22c3fab6d5e720a4d06b735e7d7c25d0757b18ca
luigiolivi <eterowiczluigi@gmail.com>
2023-06-15 20:44:22 -0300

    bcrypt incremented

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,17 +8,9 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
-      password_hash: Yup.string().required().min(6),
+      password: Yup.string().required().min(6),
       admin: Yup.boolean(),
     })
-
-    // COMENTAR LINHAS SELECIONADAS CTRL + ;
-    // if (!(await schema.isValid(request.body))) {
-    //   return response
-    //     .status(400)
-    //     .json({ error: 'Make sure your data is correct' })
-    // }
-
 
     try {
       await schema.validateSync(request.body, { abortEarly: false })
@@ -26,13 +18,21 @@ class UserController {
       return response.status(400).json({ error: err.errors })
     }
 
-    const { name, email, password_hash, admin } = request.body
+    const { name, email, password, admin } = request.body
+
+    const userExists = await User.findOne({
+      where: { email },
+    })
+
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists.' })
+    }
 
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     })
 

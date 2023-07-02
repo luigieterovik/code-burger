1c53e76b16774194e3838b2cbe459da60c26aece
luigiolivi <eterowiczluigi@gmail.com>
2023-07-01 22:01:12 -0300

    Order controller

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,71 @@
+import * as Yup from 'yup'
+import Product from '../models/Product'
+import Category from '../models/Category'
+import { model } from 'mongoose'
+
+class OrderController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      products: Yup.array()
+        .required()
+        .of(
+          Yup.object().shape({
+            id: Yup.number().required(),
+            quantity: Yup.number().required(),
+          })
+        ),
+    })
+
+    console.log(request)
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const productsId = request.body.products.map((product) => product.id)
+
+    const updatedProducts = await Product.findAll({
+      where: {
+        id: productsId,
+      },
+      include: [
+        {
+          model: Category,
+          as: 'category',
+          attributes: ['name'],
+        },
+      ],
+    })
+
+    const editedProducts = updatedProducts.map((product) => {
+      const productIndex = request.body.products.findIndex(
+        (requestProduct) => requestProduct.id === product.id
+      )
+
+      const newProduct = {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        category: product.category.name,
+        url: product.url,
+        quantity: request.body.products[productIndex].quantity,
+      }
+
+      return newProduct
+    })
+
+    const order = {
+      user: {
+        id: request.userId,
+        name: request.userName,
+      },
+      products: editedProducts,
+    }
+
+    return response.status(201).json(editedProducts)
+  }
+}
+
+export default new OrderController()
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import ProductController from './app/controllers/ProductController'
 import CategoryController from './app/controllers/CategoryController'
+import OrderController from './app/controllers/OrderController'
 
 const upload = multer(multerConfig)
 
@@ -24,5 +25,7 @@ routes.get('/products', ProductController.index)
 
 routes.post('/categories', CategoryController.store)
 routes.get('/categories', CategoryController.index)
+
+routes.post('/orders', OrderController.store)
 
 export default routes
